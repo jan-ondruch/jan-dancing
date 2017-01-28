25420b6a1f7121fd3868796181cf29b6262f4d00
jan <h.ondruch@seznam.cz>
2017-01-28 16:31:49 +0100

    git macro commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /* the initial $(function(){} prevents jQuery code running before the whole document is loaded */
 $(function(){
 
+	var clicks = true;
+
 	var items = [
 		"It all started in the summer of 2012 in Berlin.",
 		"As a kinda joke I tried my first dancing workshop.",
@@ -15,51 +17,46 @@ $(function(){
     
     $(".btn-start").click(function(){
 
-    	/* first remove the button */
-		$(".btn-start").animate({opacity:0}, (delay*200),function(){
-		    $(this).animate({opacity:0});
-		})
+  		if (clicks) {
+  		   /* first remove the button */
+			$(".btn-start").animate({opacity:0}, (delay*200),function(){
+			    $(this).animate({opacity:0});
+			})
+	
+			/* iterate through the items array to display the whole story */
+			function loop(delay) {
+			    $.each(items, function (i, elm) {
+			    	// sync the first transition with the button
+			    	if (i == 0) {
+			    		$text.delay(delay*50).fadeOut();
+			    	}	
+			    	else {
+			    		$text.delay(delay*1E3).fadeOut();	//1E3 == 1000
+			    	}
+			        
+			     	$text.delay(delay*400)	// delay transition between texts
+			        $text.queue(function(){
+			            $text.html(items[i]);
+			            $text.dequeue();
+			        });
+			        $text.fadeIn();
+			        $text.queue(function(){
+			            if (i == items.length -1) {
+			            	$(".btn-start").animate({opacity:1}, 1500).find("h2").text("Browse dancing portfolio")
+			            }
+			            $text.dequeue();
+			        });
+			    });
+			}
+	
+			loop(delay);
 
-		/* iterate through the items array to display the whole story */
-		function loop(delay) {
-		    $.each(items, function (i, elm) {
-		    	// sync the first transition with the button
-		    	if (i == 0) {
-		    		$text.delay(delay*50).fadeOut();
-		    	}	
-		    	else {
-		    		$text.delay(delay*1E3).fadeOut();	//1E3 == 1000
-		    	}
-		        
-		     	$text.delay(delay*400)	// delay transition between texts
-		        $text.queue(function(){
-		            $text.html(items[i]);
-		            $text.dequeue();
-		        });
-		        $text.fadeIn();
-		        $text.queue(function(){
-		            if (i == items.length -1) {
-		            	$(".btn-start").animate({opacity:1}, 1500).find("h2").text("Browse dancing portfolio")
-		            }
-		            $text.dequeue();
-		        });
-		    });
-		}
-
-		loop(delay);
+  		} else {
+  		   window.location = "./portfolio.html" + this.id;
+  		}
+  		// switch for the second click
+  		clicks = !clicks;
 
     });
-});
 
-/*
-$(".btn-start").click(function(){
-		$(".btn-start").animate({opacity:0},function(){
-		    $(this).animate({opacity:0});
-		})
-		for (i = 0; i < story.length; i++) {
-		    $(".intro-container").find("h1").animate({opacity:0},function(){
-			    $(this).delay(400).text(story[i]).animate({opacity:1}, 1500);
-			})
-		}	
-	});
-*/
+});
